3cbf698ba822bba1bbbfa4f86369090daf6e8b44
kenedy kipkirui <kipkiruikenedy@gmail.com>
2023-06-06 02:16:09 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css"
+import { Home } from "./components/Home"
+import { Sidebar } from "./components/Sidebar"
+
+function App() {
+  return (
+    <>
+      <Home />
+      <Sidebar />
+    </>
+  )
+}
+
+export default App
